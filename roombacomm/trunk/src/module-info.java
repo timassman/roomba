c627776d22d65cebf6f4c5fd37bfbf0fcac84863
Tim Assman <t.m.assman@gmail.com>
2020-02-02 22:01:11 +0100

    Reduced project compliance level from Java 13 to Java 7.

--- roombacomm/trunk/src/module-info.java
+++
@@ -1,9 +0,0 @@
-module roombaCommXboxController {
-	exports jargs.examples.gnu;
-	exports com.hackingroomba.roombacomm;
-	exports com.hackingroomba.roombacomm.net;
-
-	requires RXTXcomm;
-	requires jargs;
-	requires java.desktop;
-}
